b9a256c167e8e327cc16e783c87e0e31fb04d24d
jakiealarcon+1-at-860736874890 <alarconreyesj@gmail.com>
2023-10-18 22:56:24 -0500

    feat: adding properties

--- microservicio-oauth/src/main/java/pe/com/app/oauth/security/AuthorizationServerConfig.java
+++ microservicio-oauth/src/main/java/pe/com/app/oauth/security/AuthorizationServerConfig.java
@@ -3,8 +3,10 @@ package pe.com.app.oauth.security;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -12,53 +14,55 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+
+@RefreshScope
 @Configuration
 @EnableAuthorizationServer
-public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAdapter{
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	private InfoAdicionalToken aditionalToken;
-	
+
 	@Autowired
 //	@Qualifier("authenticationManager")
 	private AuthenticationManager authentificationManager;
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		//  security -> permite dar seguridad a los endpoints
-		
-		security.tokenKeyAccess("permitAll()")
-		.checkTokenAccess("isAuthenticated()");
+		// security -> permite dar seguridad a los endpoints
+
+		security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
 	}
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("frontendapp")
-		.secret(passwordEncoder.encode("123456"))
-		.scopes("read", "write")
-		.authorizedGrantTypes("password", "refresh_token")
-		.accessTokenValiditySeconds(3600)
-		.refreshTokenValiditySeconds(3600);
-		
+//		System.out.println("id -> " + env.getProperty("config.security.oauth.client.id"));
+//		System.out.println("secret -> " + env.getProperty("config.security.oauth.client.secret"));
+
+		clients.inMemory().withClient(env.getProperty("config.security.oauth.client.id"))
+				.secret(passwordEncoder.encode(env.getProperty("config.security.oauth.client.secret")))
+				.scopes("read", "write").authorizedGrantTypes("password", "refresh_token")
+				.accessTokenValiditySeconds(3600).refreshTokenValiditySeconds(3600);
+
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
 		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(aditionalToken, accessTokenConverter()));
-		
-		endpoints.authenticationManager(authentificationManager)
-		.tokenStore(tokenStore())
-		.accessTokenConverter(accessTokenConverter())
-		.tokenEnhancer(tokenEnhancerChain);
+
+		endpoints.authenticationManager(authentificationManager).tokenStore(tokenStore())
+				.accessTokenConverter(accessTokenConverter()).tokenEnhancer(tokenEnhancerChain);
 	}
 
 	@Bean
@@ -68,8 +72,10 @@ public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAda
 
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
+//		System.out.println("key -> " + env.getProperty("config.security.oauth.jwt.key"));
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("algun_codigo_secreto_aeiou");
+		// env.getProperty("config.security.oauth.jwt.key")
+		tokenConverter.setSigningKey(env.getProperty("config.security.oauth.jwt.key"));
 		return tokenConverter;
 	}
 }

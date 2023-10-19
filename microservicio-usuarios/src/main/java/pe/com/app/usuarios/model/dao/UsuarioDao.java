9aa282548b09968fde13c9b8e9d0829a32452f8b
jakiealarcon+1-at-860736874890 <alarconreyesj@gmail.com>
2023-10-18 22:59:45 -0500

    feat: conection to db

--- microservicio-usuarios/src/main/java/pe/com/app/usuarios/model/dao/UsuarioDao.java
+++ microservicio-usuarios/src/main/java/pe/com/app/usuarios/model/dao/UsuarioDao.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import pe.com.app.commons.usuarios.model.entity.Usuario;
 
@@ -12,7 +13,7 @@ import pe.com.app.commons.usuarios.model.entity.Usuario;
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long> {
 
 	@RestResource(path = "buscar-username")
-	public Usuario findByUsername(@Param("username") String username);
+	public Usuario findByUsername(@RequestParam("username") String username);
 
 	@Query("select u from Usuario u where u.username=?1")
 	public Usuario ontenerPorUsername(String username);
--- microservicio-usuarios/src/main/resources/import.sql
+++ microservicio-usuarios/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO `usuarios` (username, password, enabled, name, last_name, email) VALUES ('jakie','1234',1, 'Jakie', 'Alarcon','alarcon@gmail.com');
-INSERT INTO `usuarios` (username, password, enabled, name, last_name, email) VALUES ('admin','3434',1, 'John', 'Doe','jhon.doe@bolsadeideas.com');
+INSERT INTO `usuarios` (username, password, enabled, name, last_name, email) VALUES ('jakie','$2a$10$adAMOSetVzMlKzjobW6MceZA8yj1UUBv0DC4IYBDYZ7KI6kIvEJeu',1, 'Jakie', 'Alarcon','alarcon@gmail.com');
+INSERT INTO `usuarios` (username, password, enabled, name, last_name, email) VALUES ('admin','$2a$10$KfJeiWdbyeEGZwqQ4lK7ke8DQox6d959UdiHG4RNN211RsEjBPrUu',1, 'John', 'Doe','jhon.doe@bolsadeideas.com');
 
 INSERT INTO `roles` (name) VALUES ('ROLE_USER');
 INSERT INTO `roles` (name) VALUES ('ROLE_ADMIN');
